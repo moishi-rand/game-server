42310ef423e4be809d4339ea6d3920ead9e4f180
moishi-rand <randmeb@gmail.com>
2023-07-23 23:30:45 +0300

    init swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller()
+@ApiTags('my_api')
+@Controller('my-api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,24 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('My game server API')
+    .setDescription('Simple API for game server')
+    .setVersion('1.0')
+    .addTag('my_api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
